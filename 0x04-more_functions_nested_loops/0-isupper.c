54ca4882e4574fe0770d5120ca45edd6b1bafa4e
254jack <taixonj@gmail.com>
2023-03-23 02:46:29 -0700

    add 0-isppuer.c file

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,14 @@
-#include <main.h>
+#include "main.h"
+
 /**
- * _isupper - checks for uppercase char.
- * @c: c- Variable
- * Return: 1 success else 0
+ * _isupper -  checks for uppercase character.
+ * @c: c -  Variable
+ * Return: Always 0.
  */
 int _isupper(int c)
 {
-	if(c >= 65 && c <= 90)
+
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
