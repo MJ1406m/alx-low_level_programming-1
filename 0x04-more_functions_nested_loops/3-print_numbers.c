3b66e99c00631611cdee55b78986d3cadd16a641
254jack <taixonj@gmail.com>
2023-03-23 04:14:06 -0700

    add 3-print_numbers.c file

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * print_numers - checks for a digit in 0 through 9.
+ * print_numbers -  checks for checks for a digit (0 through 9).
  *
- * Return:0
+ * Return: Always 0.
  */
 void print_numbers(void)
 {
 	int a;
-	for (a = 48; a <58; a++)
+
+	for (a = 48; a < 58; a++)
 	{
 		_putchar(a);
 	}
-	_putchar('\n);
+	_putchar('\n');
 }
