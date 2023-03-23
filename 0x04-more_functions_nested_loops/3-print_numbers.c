cae3efc68b20e1a57e9f263ec29a0618647089e8
254jack <taixonj@gmail.com>
2023-03-23 03:58:37 -0700

    add 3-print_numbers.c file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numers - checks for a digit in 0 through 9 and prints a new line
+ *
+ * Return:0
+ */
+void print_numbers(void)
+{
+	int a;
+	for (a = 48; a <58; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n);
+}
