9382634a6476dbde01272a06622b91666cb01be6
254jack <taixonj@gmail.com>
2023-03-23 10:49:20 -0700

    add 4-print_most_numbers.c file

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,22 +1,21 @@
 #include "main.h"
+
 /**
- * print_most_numbers - prints the numbers from 0 to 9
+ * print_most_numbers -  checks for checks for a digit (0 through 9).
  *
- * Return: 0
+ * Return: Always 0.
  */
 void print_most_numbers(void)
 {
 	int a;
-	
+
 	for (a = 48; a < 58; a++)
 	{
-		
 		if (a != 50)
 		{
-			
 			if (a != 52)
 			{
-				_putchar(a);
+			_putchar(a);
 			}
 		}
 	}
