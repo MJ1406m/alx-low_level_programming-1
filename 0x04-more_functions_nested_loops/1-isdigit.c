ac2e281d671c743b3d9fce13426a13d690d2c633
254jack <taixonj@gmail.com>
2023-03-23 03:12:44 -0700

    add 1-isdigit.c file

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include <main.h>
+/**
+ * _isdigit - scans digits in 0 through 9
+ * Return: 1 if success else 0
+ */
+int _isdigit(int c)
+{
+	if(c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	return (0);
+}
