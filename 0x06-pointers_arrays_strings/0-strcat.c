5e45741253ba461e525d8c01fdb5c4ff8a69510a
Jack Taixon <91873720+254jack@users.noreply.github.com>
2023-03-30 21:12:24 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,12 +10,20 @@
 */
 char *_strcat(char *dest, char *src)
 {
-  int i, j;
-  
-  for(i = 0; src[j] != '\0'; j++)
-  {
-    dest[i] = src[j];
-    i++;
-  }
-  return (dest);
+	int lengthD, lengthS;
+
+	lengthD = 0;
+	lengthS = 0;
+
+	while (*(dest + lengthD) != '\0')
+		lengthD++;
+
+	while (*(src + lengthS) != '\0' && lengthD < 97)
+	{
+		*(dest + lengthD) = *(src + lengthS);
+		lengthD++;
+		lengthS++;
+	}
+	*(dest + lengthD) = '\0';
+	return (dest);
 }
